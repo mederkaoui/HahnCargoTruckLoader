8ce80d3fa52031c04ef78139932f1f0bd1f2f004
Christian Crede <christian.crede@hahn-software.io>
2024-07-30 12:16:05 +0200

    Add option to read trucks/crates from file

---
+++ HahnCargoTruckLoader/Helper/FileHelper.cs
@@ -0,0 +1,49 @@
+﻿using HahnCargoTruckLoader.Model;
+using System;
+using System.Collections.Generic;
+using System.Globalization;
+using System.Linq;
+using System.Text;
+using System.Text.Json;
+using System.Threading.Tasks;
+
+namespace HahnCargoTruckLoader.Helper
+{
+  public class FileHelper
+  {
+    
+
+    public static async void WriteToFile(string callerId, string content, string fileName = "")
+    {
+      var rootPath = Environment.GetFolderPath(Environment.SpecialFolder.MyDocuments);
+      if (fileName == "" || fileName == "\"\"")
+      {
+        fileName = "HahnCargoTruckLoader" + callerId + ".json";
+      }
+      await using var streamWriter = new StreamWriter(Path.Combine(rootPath, fileName), false);
+      await streamWriter.WriteLineAsync(content);
+    }
+
+    public static Truck? LoadTruckFromJson()
+    {
+      var path = Path.Combine(Environment.GetFolderPath(Environment.SpecialFolder.MyDocuments), "HahnCargoTruckLoaderTruck.json");
+
+      if (!File.Exists(path)) return null;
+
+      var jsonString = File.ReadAllText(path);
+      return JsonSerializer.Deserialize<Truck>(jsonString)!;
+    }
+
+    public static List<Crate>? LoadCratesFromJson()
+    {
+      var path = Path.Combine(Environment.GetFolderPath(Environment.SpecialFolder.MyDocuments), "HahnCargoTruckLoaderCrates.json");
+
+      if (!File.Exists(path)) return null;
+
+      var jsonString = File.ReadAllText(path);
+      return JsonSerializer.Deserialize<List<Crate>>(jsonString)!;
+    }
+
+
+  }
+}
--- HahnCargoTruckLoader/Logic/Initialize.cs
+++ HahnCargoTruckLoader/Logic/Initialize.cs
@@ -1,8 +1,10 @@
-﻿using HahnCargoTruckLoader.Model;
+﻿using HahnCargoTruckLoader.Helper;
+using HahnCargoTruckLoader.Model;
 using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
+using System.Text.Json;
 using System.Threading.Tasks;
 
 namespace HahnCargoTruckLoader.Logic
@@ -11,20 +13,34 @@ namespace HahnCargoTruckLoader.Logic
   {
     public static Truck LoadTruck()
     {
-      return new Truck { TruckType = "H-01", Height = 2, Width = 3, Length = 4 }; //24
+      Truck? truck = FileHelper.LoadTruckFromJson();
+      if(truck == null)
+      {
+        truck = new Truck { TruckType = "H-01", Height = 2, Width = 3, Length = 4 }; //24
+        FileHelper.WriteToFile("Truck", JsonSerializer.Serialize(truck));
+      }
+      
+      return truck;
     }
 
     public static List<Crate> GetCrates()
     {
-      var crates = new List<Crate>();
-      crates.Add(new Crate { CrateID = 0, Height = 2, Width = 1, Length = 4 }); //8
-      crates.Add(new Crate { CrateID = 1, Height = 1, Width = 2, Length = 2 }); //4
-      crates.Add(new Crate { CrateID = 2, Height = 2, Width = 2, Length = 1 }); //4
-      crates.Add(new Crate { CrateID = 3, Height = 1, Width = 1, Length = 2 }); //2
-      crates.Add(new Crate { CrateID = 4, Height = 1, Width = 2, Length = 1 }); //2
-      crates.Add(new Crate { CrateID = 5, Height = 1, Width = 1, Length = 1 }); //1
-      crates.Add(new Crate { CrateID = 6, Height = 1, Width = 1, Length = 1 }); //1
-      crates.Add(new Crate { CrateID = 7, Height = 1, Width = 2, Length = 2 }); //2
+      List<Crate>? crates = FileHelper.LoadCratesFromJson();
+      if (crates == null)
+      {
+        crates =
+        [
+          new Crate { CrateID = 0, Height = 2, Width = 1, Length = 4 }, //8
+          new Crate { CrateID = 1, Height = 1, Width = 2, Length = 2 }, //4
+          new Crate { CrateID = 2, Height = 2, Width = 2, Length = 1 }, //4
+          new Crate { CrateID = 3, Height = 1, Width = 1, Length = 2 }, //2
+          new Crate { CrateID = 4, Height = 1, Width = 2, Length = 1 }, //2
+          new Crate { CrateID = 5, Height = 1, Width = 1, Length = 1 }, //1
+          new Crate { CrateID = 6, Height = 1, Width = 1, Length = 1 }, //1
+          new Crate { CrateID = 7, Height = 1, Width = 2, Length = 2 }, //2
+        ];
+        FileHelper.WriteToFile("Crates", JsonSerializer.Serialize(crates));
+      }
 
       return crates;
     }
